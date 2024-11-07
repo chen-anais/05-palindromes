b1940a3d496c51cb3accf5062f05d31f885114dd
chen-anais <anais.chen@edu.esiee.fr>
2024-11-07 10:35:02 +0000

    Chen Anais, exercice palindromes

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+"""
+    contient une fonction ispalindrome 
+    qui retourne si la string passée en paramètre est un palindrome
+    et un fonction main qui fait appel à ispalindrome
+"""
+import string
+from unidecode import unidecode
 #### Fonction secondaire
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
-
+    """
+        retourne si la string passée en paramètre est un palindrome
+    """
+    p_min = p.lower()
+    p_sans_espace = p_min.replace(" ","")
+    p_sans_accent = unidecode(p_sans_espace)
+    p_final = p_sans_accent.translate(str.maketrans("","", string.punctuation))
+    return p_final == p_final[::-1]
 #### Fonction principale
 
 
 def main():
+    """
+    fait appel à la fonction ispalindrome
+    """
 
     # vos appels à la fonction secondaire ici
 
